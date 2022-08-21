3e2def95f2036acba6669bec7b766979a2b84374
Denis <96013977+xdeanboy@users.noreply.github.com>
2022-08-22 01:38:39 +0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,2 @@
-<!--<script>
-    $("#btnBoss").click(function (){
-        var cardByBoss = '<div class="card"><h4 class="title">Card title</h4><div class="text">Someone text</div></div>';
-        $("#boss-cards").append(cardByBoss)
-    })
-
-    $("#btnManager").click(function (){
-        var cardByManager = '<div class="card"><h4 class="title">Card title</h4><div class="text">Someone text</div></div>';
-        $("#manager-cards").append(cardByManager)
-    })
-
-    $("#btnPerformer").click(function (){
-        var cardByPerformer = '<div class="card"><h4 class="title">Card title</h4><div class="text">Someone text</div></div>';
-        $("#performer-cards").append(cardByPerformer)
-    })
-</script>-->
 </body>
 </html>
